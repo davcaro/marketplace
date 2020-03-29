cfe7717bae36adebd2ea2d56a6b74ae23ab75d32
David C <carogallegodavid@gmail.com>
2020-03-29 18:33:36 +0200

    Add logger config

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const logger = require('morgan');
+const morgan = require('morgan');
+const logger = require('./config/logger');
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(morgan('combined', { stream: logger.stream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
