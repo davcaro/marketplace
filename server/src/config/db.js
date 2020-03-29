cb82a3c0e93ea7ae43c79d5dd598905909fac0bb
David C <carogallegodavid@gmail.com>
2020-03-29 18:44:32 +0200

    Add Sequelize and initial config

---
+++ server/src/config/db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const {
+  ENV: { DB }
+} = require('./index');
+
+module.exports = {
+  development: {
+    username: DB.username,
+    password: DB.password,
+    database: DB.database,
+    host: DB.host,
+    port: DB.port,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  }
+};
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,6 +1,13 @@
 module.exports = {
   ENV: {
     NODE_ENV: process.env.NODE_ENV || 'development',
-    NODE_PORT: process.env.NODE_PORT || process.env.PORT || 3000
+    NODE_PORT: process.env.NODE_PORT || process.env.PORT || 3000,
+    DB: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT
+    }
   }
 };
