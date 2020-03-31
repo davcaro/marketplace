2dc05906faaf5fa6108b1ed6e868d675ea5e530b
David C <carogallegodavid@gmail.com>
2020-03-31 18:20:50 +0200

    Upgrade auth responses data

--- server/src/api/features/auth/auth-controller.js
+++ server/src/api/features/auth/auth-controller.js
@@ -18,9 +18,7 @@ const login = async (req, res, next) => {
 
         const body = {
           _id: user.id,
-          user: user.email,
-          name: user.name,
-          picture: user.picture
+          user: user.email
         };
 
         // Need to append "s" to get time in seconds.
@@ -29,7 +27,14 @@ const login = async (req, res, next) => {
         // Generate the JWT token
         const token = jwt.sign(body, config.ENV.JWT_KEY, { expiresIn });
 
-        return res.json({ token });
+        return res.json({
+          _id: user.id,
+          email: user.email,
+          name: user.name,
+          picture: user.picture,
+          token,
+          token_ttl: config.ENV.JWT_TTL
+        });
       });
     } catch (error) {
       return next(error);
@@ -39,17 +44,46 @@ const login = async (req, res, next) => {
 
 const signUp = async (req, res, next) => {
   try {
-    const user = await authService.createUser(req.body);
-
-    return res.json({
-      _id: user.id,
-      user: user.email,
-      name: user.name,
-      picture: user.picture
-    });
+    await authService.createUser(req.body);
   } catch (e) {
     return next(e);
   }
+
+  return passport.authenticate('login', async (err, user) => {
+    try {
+      if (err || !user) {
+        return next(err);
+      }
+
+      return req.logIn(user, { session: false }, error => {
+        if (error) {
+          return next(error);
+        }
+
+        const body = {
+          _id: user.id,
+          user: user.email
+        };
+
+        // Need to append "s" to get time in seconds.
+        const expiresIn = `${config.ENV.JWT_TTL}s`;
+
+        // Generate the JWT token
+        const token = jwt.sign(body, config.ENV.JWT_KEY, { expiresIn });
+
+        return res.json({
+          _id: user.id,
+          email: user.email,
+          name: user.name,
+          picture: user.picture,
+          token,
+          token_ttl: config.ENV.JWT_TTL
+        });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
 };
 
 module.exports = {
