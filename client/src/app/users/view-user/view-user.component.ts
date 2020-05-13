4093743c1c88a2b1b736776b2d86a240d2b7af56
David C <carogallegodavid@gmail.com>
2020-05-13 11:31:31 +0200

    Show location map and user reviews in View user component

--- client/src/app/users/view-user/view-user.component.ts
+++ client/src/app/users/view-user/view-user.component.ts
@@ -1,28 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnInit } from '@angular/core';
 import { User } from '../user.model';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { ReviewsService } from 'src/app/reviews/reviews.service';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-view-user',
   templateUrl: './view-user.component.html',
   styleUrls: ['./view-user.component.less']
 })
-export class ViewUserComponent {
+export class ViewUserComponent implements OnInit, AfterViewInit {
   apiUrl: string;
   user: User;
+  userScore: { score: number; reviews: number };
 
   forSaleItemsCount: number;
   soldItemsCount: number;
   reviewsCount: number;
 
-  constructor(private route: ActivatedRoute) {
+  @ViewChild('mapTab', { static: false }) mapTabEl: ElementRef;
+  @ViewChild('mapSide', { static: false }) mapSideEl: ElementRef;
+  mapTab: mapboxgl.Map;
+  mapSide: mapboxgl.Map;
+  mapbox = mapboxgl as typeof mapboxgl;
+
+  constructor(private route: ActivatedRoute, private reviewsService: ReviewsService) {
     this.apiUrl = environment.apiUrl;
     this.user = this.route.snapshot.data.user;
 
     this.forSaleItemsCount = 0;
     this.soldItemsCount = 0;
     this.reviewsCount = 0;
+    this.userScore = { score: 0, reviews: 0 };
+  }
+
+  ngOnInit(): void {
+    this.reviewsService.getUserScore(this.user.id).subscribe(res => {
+      this.userScore = res;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.mapbox.accessToken = environment.mapbox.accessToken;
+
+    // Side map
+    this.mapSide = new mapboxgl.Map({
+      container: this.mapSideEl.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.user.location.longitude, this.user.location.latitude],
+      zoom: this.user.location.zoom
+    });
+
+    this.mapSide.addControl(new mapboxgl.NavigationControl());
+    new mapboxgl.Marker().setLngLat([this.user.location.longitude, this.user.location.latitude]).addTo(this.mapSide);
   }
 
   updateForSaleItemsCount(count: number) {
@@ -35,5 +66,21 @@ export class ViewUserComponent {
 
   updateReviewsCount(count: number) {
     this.reviewsCount = count;
+  }
+
+  loadTabMap(tab: number) {
+    if (tab === 3 && !this.mapTab) {
+      setTimeout(() => {
+        this.mapTab = new mapboxgl.Map({
+          container: this.mapTabEl.nativeElement,
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [this.user.location.longitude, this.user.location.latitude],
+          zoom: this.user.location.zoom
+        });
+
+        this.mapTab.addControl(new mapboxgl.NavigationControl());
+        new mapboxgl.Marker().setLngLat([this.user.location.longitude, this.user.location.latitude]).addTo(this.mapTab);
+      }, 0);
+    }
   }
 }
