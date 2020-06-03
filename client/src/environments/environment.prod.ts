78bafd099517c2f6f181afab4ad3c905ff545b44
David C <carogallegodavid@gmail.com>
2020-06-03 17:12:08 +0200

    Update production environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://marketplace-finalproject.herokuapp.com/',
+  socketUrl: 'https://marketplace-finalproject.herokuapp.com/',
+  currencyLocale: 'ES-ES',
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoibWFya2V0cGxhY2UiLCJhIjoiY2thZmZ1OXZ2MjVkNjJ4czlneGo5YjN5YiJ9.36OS_JNQMRuebTQgAcLtWQ'
+  },
+  analytics: { id: 'UA-167314678-1' }
 };
